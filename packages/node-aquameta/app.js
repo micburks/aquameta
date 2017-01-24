090ae2250051f6f891dbac47558043f98487cf9b
Mickey Burks <mickey@aquameta.com>
2017-01-24 13:22:01 -0600

    Minimize amount of work needed in consumer app

--- packages/node-aquameta/app.js
+++ packages/node-aquameta/app.js
@@ -3,52 +3,36 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
 
-// Set up the express app
 const app = express();
-// TODO dont need both, I dont think
-app.use(cookieParser());
 
-
-// session
-let sess = {
-    secret: 'keyboard cat',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true
-};
-
-if (app.get('env') === 'production') {
-    app.set('trust proxy', 1); // trust first proxy
-    sess.cookie.secure = true; // serve secure cookies
-}
-app.use(session(sess))
-
-// Log request to the console
 app.use(logger('dev'));
 app.set('views', './views');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('dist'));
-app.use(express.static('public'));
-
-// Client-side usage
+/* Client-side usage */
 /**********************************************************/
-// Optional Configuration
+/* Optional Configuration */
 const endpointConfig = {
     url: '/endpoint',
-    version: 'v1'
+    version: 'v1',
+    sessionCookie: 'SESSION_ID'
 };
-// Register data routes
+
+/* Register data routes */
 const datum = require('./endpoint')(app, endpointConfig);
 /**********************************************************/
 
-// Server-side usage
+
+/* REQUIRED if using Server-Side API */
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/* Server-side usage */
 /**********************************************************/
-// Pass endpoint into app-specific routes to use
+/* Pass endpoint into app-specific routes to use */
 require('./server/routes')(app, datum);
 /**********************************************************/
 
