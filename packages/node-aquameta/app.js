9f93f1915a443a949181e9f89dabe82fd1963992
Mickey Burks <mickey@aquameta.com>
2017-04-08 10:33:52 -0500

    Server works

--- packages/node-aquameta/app.js
+++ packages/node-aquameta/app.js
@@ -1,5 +1,6 @@
 'use strict'
-const aquameta = require('node-aquameta')
+//const aquameta = require('node-aquameta')
+const aquameta = require('./src/index.js')
 const express = require('express')
 const logger = require('morgan')
 
@@ -18,22 +19,25 @@ app.set('views', './views')
 /* Optional Configuration */
 const endpointConfig = {
   url: '/endpoint',
-  version: 'v1',
-  sessionCookie: 'SESSION_ID'
+  version: 'v0.1'
 }
 
 /* Register data routes */
 const datum = aquameta.routes(endpointConfig, app)
 
 /* Pass endpoint into app-specific routes to use */
-require('./server/routes')(app, datum)
+//require('./server/routes')(app, datum)
 
 /* Use in routes */
 // datum(request).schema('').table('').rows()
 /**********************************************************/
 
 /* If you want a role-verified node-postgres connection */
-//const db = aquameta.connect(request).then().catch()
+// const db = aquameta.connect(request).then().catch()
+
+/* If you want to do something different with server-side notifications */
+// endpointConfig = { sockets: false }
+// aquameta.connect.then(client => client.on('notification'))
 
 
 module.exports = app
