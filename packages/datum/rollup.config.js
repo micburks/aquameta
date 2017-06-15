0c997d8379b952f5870808af0b637b3dc9f9eea2
Mickey Burks <mickey@aquameta.com>
2017-06-14 23:37:08 -0500

    Update rollup build

--- packages/datum/rollup.config.js
+++ packages/datum/rollup.config.js
@@ -1,5 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
+import eslint from 'rollup-plugin-eslint'
+import uglify from 'rollup-plugin-uglify'
 
 export default {
   entry: 'src/index.js',
@@ -7,11 +9,13 @@ export default {
   dest: 'dist/build.js',
   moduleName: 'datum',
   plugins: [
-    babel(),
+    eslint(),
     resolve({
       jsnext: true,
       main: true,
       extensions: [ '.js' ]
-    })
+    }),
+    babel(),
+    uglify()
   ]
 }
