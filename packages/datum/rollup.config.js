ed8105c9397d6a043bc6462cf422b05e67174682
Mickey Burks <mickey@aquameta.com>
2017-06-16 01:07:40 -0500

    Build to different dist files

--- packages/datum/rollup.config.js
+++ packages/datum/rollup.config.js
@@ -1,21 +1,21 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import eslint from 'rollup-plugin-eslint'
-import uglify from 'rollup-plugin-uglify'
+const pkg = require('./package.json')
 
 export default {
   entry: 'src/index.js',
-  format: 'umd',
-  dest: 'dist/build.js',
-  moduleName: 'datum',
+  targets: [
+    { dest: pkg.main, format: 'cjs' },
+    { dest: pkg.module, format: 'es' }
+  ],
   plugins: [
     eslint(),
     resolve({
-      jsnext: true,
       main: true,
+      jsnext: true,
       extensions: [ '.js' ]
     }),
-    babel(),
-    uglify()
+    babel()
   ]
 }
