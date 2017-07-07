64591c334806dd56d82f378af2feedf3cdcab5c8
Mickey Burks <brks.mck@gmail.com>
2017-07-07 08:35:16 -0500

    Update build process with multiple rollup configs

--- packages/datum/rollup.config.js
+++ packages/datum/rollup.config.js
@@ -1,21 +1,26 @@
+import { readFileSync } from 'fs'
 import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
 import eslint from 'rollup-plugin-eslint'
-const pkg = require('./package.json')
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
+
+const banner = readFileSync('./banner.js', 'utf-8')
+  .replace('${version}', pkg.version)
 
 export default {
   entry: 'src/index.js',
+  plugins: [
+    eslint(),
+    babel()
+  ],
+  external: [
+    'aquameta-query'
+  ],
+  banner,
+  sourceMap: true,
+  moduleName: 'datum',
   targets: [
     { dest: pkg.main, format: 'cjs' },
     { dest: pkg.module, format: 'es' }
-  ],
-  plugins: [
-    eslint(),
-    resolve({
-      main: true,
-      jsnext: true,
-      extensions: [ '.js' ]
-    }),
-    babel()
   ]
 }
