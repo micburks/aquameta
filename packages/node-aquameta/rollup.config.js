01c06162ff4e9316d7f08696773bf7ce0b8f20d1
Mickey Burks <mickey@burks.online>
2017-07-11 00:22:26 -0500

    Use rollup over webpack

---
+++ packages/node-aquameta/rollup.config.js
@@ -0,0 +1,26 @@
+import { readFileSync } from 'fs'
+import babel from 'rollup-plugin-babel'
+import eslint from 'rollup-plugin-eslint'
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
+
+const banner = readFileSync('./banner.js', 'utf-8')
+  .replace('${version}', pkg.version)
+
+export default {
+  entry: 'src/index.js',
+  plugins: [
+    eslint(),
+    babel()
+  ],
+  external: [
+    'aquameta-query'
+  ],
+  banner,
+  sourceMap: true,
+  moduleName: 'aquameta',
+  targets: [
+    { dest: pkg.main, format: 'cjs' },
+    { dest: pkg.module, format: 'es' }
+  ]
+}
