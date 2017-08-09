7ec090ded9936b36ee9f6dd2d77735b7bfafa60a
Mickey Burks <brks.mck@gmail.com>
2017-08-08 21:11:14 -0500

    Update demo code

--- packages/node-aquameta/demo/routes/index.js
+++ packages/node-aquameta/demo/routes/index.js
@@ -1,14 +1,8 @@
-const ctrls = require('../controllers');
+const ctrls = require('../controllers')
 
-// Export a single function that attaches routes to the application
-// Associate routes with controllers
 module.exports = (app, datum) => {
-    // Contact
-    app.get('/client/contact', (req, res) => res.render('client.contact.pug'));
-
-    //app.get('/server/contact', (req, res) => res.render('server.contact.pug'));
-    app.get('/server/contact', (req, res) => ctrls.contact.get(req, res, datum));
-    app.post('/server/contact', (req, res) => ctrls.contact.post(req, res, datum));
-
-};
+  app.get('/client/contact', (req, res) => res.render('client.contact.pug'))
+  app.get('/server/contact', (req, res) => ctrls.contact.get(req, res, datum))
+  app.post('/server/contact', (req, res) => ctrls.contact.post(req, res, datum))
+}
 
