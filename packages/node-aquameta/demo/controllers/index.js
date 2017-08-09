0e968207bb0e3298273877465e3050a0318bd61f
Mickey Burks <mickey@burks.online>
2017-08-08 21:11:14 -0500

    Update demo code

--- packages/node-aquameta/demo/controllers/index.js
+++ packages/node-aquameta/demo/controllers/index.js
@@ -1,7 +1,1 @@
-const contact = require('./contact');
-
-// Bring in all controllers and attach them to a single controller object for
-// simplicity in the router
-module.exports = {
-    contact /* shorthand for 'contact: contact' */
-};
+module.exports = { contact: require('./contact') }
