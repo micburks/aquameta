abe06c8c900d2b081693c6c4993fade42c84326f
Mickey Burks <brks.mck@gmail.com>
2018-02-18 12:18:12 -0600

    Build with datum, fix import error with isomorphic-fetch, fix babel issues

--- packages/widget/rollup.config.js
+++ packages/widget/rollup.config.js
@@ -1,9 +1,18 @@
-const external = require('repl')._builtinLibs.concat('aquameta-datum')
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+
+const external = require('repl')._builtinLibs.concat([
+  'isomorphic-fetch'
+])
 
 export default {
   entry: 'src/index.js',
   format: 'umd',
   dest: 'dist/build.js',
   moduleName: 'widget',
-  external
+  external,
+  plugins: [
+    nodeResolve(),
+    babel()
+  ]
 }
