398d8921d581a560bb4fd3b9ac961003a94f009a
Mickey Burks <mickey@burks.online>
2018-04-23 13:41:27 -0500

    More complex widget

--- packages/widget/src/app/app.html.js
+++ packages/widget/src/app/app.html.js
@@ -1,6 +1,6 @@
 export default `
   <div class="Component {{= input.variant }}">
-    <h1>{{= input.title }}</h1>
+    <h1 onClick="clickEvent">{{= input.title }}</h1>
     <p>{{= input.paragraph }}</p>
   </div>
 `
