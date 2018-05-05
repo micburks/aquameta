bd85f79ed1fa06388024a872b57ea0870abfe402
Mickey Burks <mickey@burks.online>
2018-05-05 18:41:49 -0500

    Remove webpack, use latest datum, fix babel and rollup

--- packages/widget/rollup.config.js
+++ packages/widget/rollup.config.js
@@ -1,21 +1,25 @@
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import globals from 'rollup-plugin-node-globals'
+import babel from 'rollup-plugin-babel'
 import builtins from 'rollup-plugin-node-builtins'
+import commonjs from 'rollup-plugin-commonjs'
+import globals from 'rollup-plugin-node-globals'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'src/widget.js',
   plugins: [
+    babel(),
     globals(),
     builtins(),
+    resolve({
+      browser: true,
+      only: ['ramda', 'aquameta-datum', 'scope-css', 'dot', 'parse5']
+    }),
     commonjs({
       include: 'node_modules/**',
-    }),
-    resolve()
+    })
   ],
   output: {
     file: 'dist/build.js',
-    format: 'umd',
-    name: 'widget',
+    format: 'es'
   }
 }
