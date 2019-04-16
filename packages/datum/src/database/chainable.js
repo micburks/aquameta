33701f4085ec8aac4d6857b1d7e07b74af3d4c32
Mickey Burks <mickey@burks.online>
2019-04-16 00:32:28 -0700

    Fix linting for database, add some thoughts on http vs source

--- packages/datum/src/database/chainable.js
+++ packages/datum/src/database/chainable.js
@@ -1,56 +1,56 @@
-import { __ } from 'ramda'
-import { addArg, addArrayArg, addOrder, addWhere } from './args.js'
+import {__} from 'ramda';
+import {addArg, addArrayArg, addOrder, addWhere} from './args.js';
 
-export function relation (name) {
-  let [ schemaName, relationName ] = name.split('.')
+export function relation(name) {
+  let [schemaName, relationName] = name.split('.');
   if (!relationName) {
-    relationName = schemaName
-    schemaName = 'public'
+    relationName = schemaName;
+    schemaName = 'public';
   }
 
   return {
     schemaName,
     relationName,
     url: `relation/${schemaName}/${relationName}`,
-    args: {}
-  }
+    args: {},
+  };
 }
 
 /**
  * Operations to perform on chainable (returned from relation)
  */
-export const where = addWhere('=')
-export const whereEquals = where
-export const whereNot = addWhere('<>')
-export const whereNotEquals = whereNot
-export const whereGt = addWhere('>')
-export const whereGte = addWhere('>=')
-export const whereLt = addWhere('<')
-export const whereLte = addWhere('<=')
-export const whereLike = addWhere('like')
-export const whereNotLike = addWhere('not like')
-export const whereSimilarTo = addWhere('similar to')
-export const whereNotSimilarTo = addWhere('not similar to')
-export const whereNull = addWhere('is', __, null)
-export const whereNotNull = addWhere('is not', __, null)
+export const where = addWhere('=');
+export const whereEquals = where;
+export const whereNot = addWhere('<>');
+export const whereNotEquals = whereNot;
+export const whereGt = addWhere('>');
+export const whereGte = addWhere('>=');
+export const whereLt = addWhere('<');
+export const whereLte = addWhere('<=');
+export const whereLike = addWhere('like');
+export const whereNotLike = addWhere('not like');
+export const whereSimilarTo = addWhere('similar to');
+export const whereNotSimilarTo = addWhere('not similar to');
+export const whereNull = addWhere('is', __, null);
+export const whereNotNull = addWhere('is not', __, null);
 
-export const order = addOrder
-export const orderBy = order
-export const orderByAsc = addOrder('asc')
-export const orderByDesc = addOrder('desc')
-export const orderRandom = addOrder('random()')
+export const order = addOrder;
+export const orderBy = order;
+export const orderByAsc = addOrder('asc');
+export const orderByDesc = addOrder('desc');
+export const orderRandom = addOrder('random()');
 
-export const limit = addArg('limit')
+export const limit = addArg('limit');
 
-export const offset = addArg('offset')
+export const offset = addArg('offset');
 
-export const evented = addArg('evented')
-export const metaData = addArg('metaData')
+export const evented = addArg('evented');
+export const metaData = addArg('metaData');
 
-export const exclude = addArrayArg('exclude')
-export const excludeColumn = exclude
-export const include = addArrayArg('include')
-export const includeColumn = include
+export const exclude = addArrayArg('exclude');
+export const excludeColumn = exclude;
+export const include = addArrayArg('include');
+export const includeColumn = include;
 
 // For functions... ?
-export const args = {}
+export const args = {};
--- packages/datum/src/database/executable.js
+++ packages/datum/src/database/executable.js
@@ -1,6 +1,13 @@
-import url from 'url'
-import { __, curry } from 'ramda'
-import { DELETE, EXECUTABLE, INSERT, SELECT, UPDATE, getMethodFromType } from './constants.js'
+import url from 'url';
+import {__, curry} from 'ramda';
+import {
+  DELETE,
+  EXECUTABLE,
+  INSERT,
+  SELECT,
+  UPDATE,
+  getMethodFromType,
+} from './constants.js';
 
 /**
  * DOESN'T allow chainables to be called on executables
@@ -10,40 +17,59 @@ const executable = curry((type, chainable, data) => ({
   url: chainable.url,
   args: chainable.args,
   data,
-  [EXECUTABLE]: true
-}))
+  [EXECUTABLE]: true,
+}));
 
 /**
  * turn relation into executable
  */
-export const del = executable(DELETE, __, null)
-export const insert = executable(INSERT)
-export const select = executable(SELECT, __, null)
-export const update = executable(UPDATE)
+export const del = executable(DELETE, __, null);
+export const insert = executable(INSERT);
+export const select = executable(SELECT, __, null);
+export const update = executable(UPDATE);
 
 /**
- * returns executable
+ * parse http request and return executable
+ *
+ * e.g.
+ * query(
+ *   client.connection(),
+ *   database.http(req)
+ * ).then(...).catch(...)
  */
-export function http (req) {
-  const parsed = url.parse(req.url, true)
+export function http(req) {
+  const parsed = url.parse(req.url, true);
 
   return {
     method: req.method,
     url: parsed.pathname,
     args: parsed.query || {},
     data: req.body,
-    [EXECUTABLE]: true
-  }
+    [EXECUTABLE]: true,
+  };
 }
 
 /**
- * returns executable
+ * parse source url and return executable
+ *
+ * e.g.
+ * query(
+ *   client.connection(),
+ *   database.source(req.url)
+ * ).then(...).catch(...)
+ *
+ * doesnt make sense to use this on the server, you still need other information from http request
+ * in that case, http can emcompass both functionalities and just figure out how to parse the request
+ *
+ * from the client, you wouldn't want to use datum to get files. you would just use fetch or whatever
+ * you normally use. source urls are meant to be compatible with current technologies, not accessed
+ * thorugh datum
  */
-export function source (sourceUrl) {
-  const method = 'GET'
-  const url = `/${sourceUrl.replace(/\/db\//, '')}`
-  const args = {} // ?
-  const data = {}
+export function source(sourceUrl) {
+  const method = 'GET';
+  const url = `/${sourceUrl.replace(/\/db\//, '')}`;
+  const args = {}; // ?
+  const data = {};
 
   // TODO: analyze sourceUrl to find if its row/relation/field and query args
 
@@ -52,6 +78,6 @@ export function source (sourceUrl) {
     url,
     args,
     data,
-    [EXECUTABLE]: true
-  }
+    [EXECUTABLE]: true,
+  };
 }
