22efb9a5aa20a0a9ff0a5b7ceeb74bdf591939c0
Mickey Burks <brks.mck@gmail.com>
2019-04-16 00:32:28 -0700

    Fix linting for database, add some thoughts on http vs source

--- packages/datum/src/database/constants.js
+++ packages/datum/src/database/constants.js
@@ -1,25 +1,27 @@
-import { always, cond, equals, T } from 'ramda'
+import {always, cond, equals, T} from 'ramda';
 
-export const CLIENT = Symbol('client')
-export const EXECUTABLE = Symbol('executable')
-
-export const DELETE = 'DELETE'
-export const INSERT = 'INSERT'
-export const SELECT = 'SELECT'
-export const UPDATE = 'UPDATE'
+export const DELETE = 'DELETE';
+export const INSERT = 'INSERT';
+export const SELECT = 'SELECT';
+export const UPDATE = 'UPDATE';
 
 export const HTTP = {
   GET: 'GET',
   POST: 'POST',
   PUT: 'PUT',
   PATCH: 'PATCH',
-  DELETE: 'DELETE'
-}
+  DELETE: 'DELETE',
+};
 
 export const getMethodFromType = cond([
   [equals(DELETE), always(HTTP.DELETE)],
   [equals(INSERT), always(HTTP.PATCH)],
   [equals(SELECT), always(HTTP.GET)],
   [equals(UPDATE), always(HTTP.PATCH)],
-  [T, type => { throw new TypeError(`unknown type: ${type}`) }]
-])
+  [
+    T,
+    type => {
+      throw new TypeError(`unknown type: ${type}`);
+    },
+  ],
+]);
