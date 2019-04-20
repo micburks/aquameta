7a5ea25eb7c29238fbe2dad2f8b4b3911035b0c2
Mickey Burks <brks.mck@gmail.com>
2019-04-20 15:09:22 -0700

    Add placeholders for tests that still need to be written

---
+++ packages/datum/src/__tests__/query/connection.js
@@ -0,0 +1,4 @@
+import test from 'ava';
+import connection from '../../query/connection.js';
+
+test.skip('#connection', t => {});
---
+++ packages/datum/src/__tests__/query/endpoint.js
@@ -0,0 +1,4 @@
+import test from 'ava';
+import endpoint from '../../query/endpoint.js';
+
+test.skip('#endpoint', t => {});
