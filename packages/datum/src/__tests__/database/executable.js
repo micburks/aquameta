a311bc3601801ffc3b36755b4e9d1eae506a291d
Mickey Burks <brks.mck@gmail.com>
2019-04-20 15:08:13 -0700

    Tests for http

--- packages/datum/src/__tests__/database/executable.js
+++ packages/datum/src/__tests__/database/executable.js
@@ -3,16 +3,34 @@
 import test from 'ava';
 import {database} from '../../index.js';
 
-test('http is defined', t => {
-  const mockRequest = {
-    method: 'GET',
-    url: '/endpoint/relation/test/table',
+test('#http - parses request', t => {
+  const req = {
+    method: 'POST',
+    url: '/endpoint/relation/test/table?name=value',
+    body: {body: true},
   };
 
-  const executable = database.http(mockRequest);
+  const executable = database.http(req);
+
+  t.is(executable.method, 'POST');
+  t.is(executable.url, '/endpoint/relation/test/table');
+  t.deepEqual(executable.args, {name: 'value'});
+  t.deepEqual(executable.data, {body: true});
+});
+
+test('#http - parses source url', t => {
+  const req = {
+    url: '/db/widget/widget/Component.js',
+  };
+
+  const executable = database.http(req);
 
   t.is(executable.method, 'GET');
-  t.is(executable.url, '/endpoint/relation/test/table');
-  t.deepEqual(executable.args, {});
-  t.is(executable.data, undefined);
+  t.is(executable.url, 'relation/widget/widget');
+  t.deepEqual(executable.args, {
+    source: true,
+    include: ['js'],
+    where: [{name: 'name', op: '=', value: 'Component'}],
+  });
+  t.is(executable.data, null);
 });
