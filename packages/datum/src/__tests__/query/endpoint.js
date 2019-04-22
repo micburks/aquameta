fb9c2a53875efde322125c7a277499c3f703dc59
Mickey Burks <mickey@burks.online>
2019-04-21 17:50:34 -0700

    Fix and test endpoint

--- packages/datum/src/__tests__/query/endpoint.js
+++ packages/datum/src/__tests__/query/endpoint.js
@@ -1,6 +1,89 @@
 import test from 'tape';
 import endpoint from '../../query/endpoint.js';
+import * as fetchExport from '../../query/fetch.js';
+import sinon from 'sinon';
 
-test.skip('#endpoint', t => {
+test('#endpoint', async t => {
+  const fakeFetch = (url, options) => {
+    t.is(url, '/db/v3/relation/endpoint/session');
+    t.is(options.method, 'POST');
+    t.is(options.body, undefined, 'body is not added with no data');
+    t.true('credentials' in options, 'some form of credentials are used');
+    return {
+      json: sinon.fake(),
+    };
+  };
+  sinon.stub(fetchExport, 'fetch').callsFake(fakeFetch);
+
+  const client = {
+    url: '/db/',
+    version: 'v3',
+  };
+  const query = {
+    url: '/relation//endpoint/session/',
+    method: 'POST',
+    args: {},
+    data: null,
+  };
+  await endpoint(client, query);
+  fetchExport.fetch.restore();
+  t.end();
+});
+
+test('#endpoint - with data', async t => {
+  const fakeFetch = (url, options) => {
+    t.is(typeof options.body, 'string', 'body is a string');
+    t.is(options.body, '{"key":"value"}', 'body is stringified');
+    return {
+      json: sinon.fake(),
+    };
+  };
+  sinon.stub(fetchExport, 'fetch').callsFake(fakeFetch);
+
+  const query = {
+    url: '/relation//endpoint/session/',
+    method: 'POST',
+    args: {},
+    data: {key: 'value'},
+  };
+  await endpoint({}, query);
+  fetchExport.fetch.restore();
+  t.end();
+});
+
+test('#endpoint - with args', async t => {
+  const argsString = 'limit=5&order_by=name';
+  const args = {
+    order: [
+      {
+        column: 'name',
+        direction: 'asc',
+      },
+    ],
+    limit: 5,
+  };
+  const client = {
+    url: '/db',
+    version: 'v3',
+  };
+  const query = {
+    url: 'relation/endpoint/session',
+    args,
+  };
+
+  const fakeFetch = url => {
+    t.is(
+      url,
+      `${client.url}/${client.version}/${query.url}?${argsString}`,
+      'adds query string',
+    );
+    return {
+      json: sinon.fake(),
+    };
+  };
+  sinon.stub(fetchExport, 'fetch').callsFake(fakeFetch);
+
+  await endpoint(client, query);
+  fetchExport.fetch.restore();
   t.end();
 });
--- packages/datum/src/query/endpoint.js
+++ packages/datum/src/query/endpoint.js
@@ -1,9 +1,8 @@
 // @flow
-/* eslint-env browser */
 // TODO this can also be run from node
 
-import fetch from 'unfetch';
-import type {Client, Executable, QueryResult} from '../types.js';
+import {fetch} from './fetch.js';
+import type {Client, Executable, QueryResult, WhereOps} from '../types.js';
 
 type Request = {
   method: string,
@@ -20,12 +19,10 @@ export default async function executeEndpoint(
   client: Client,
   query: Executable,
 ): Promise<QueryResult> {
-  let requestUrl = `/${client.url}/${client.version}/${query.url}`.replace(
-    /\/+/g,
-    '/',
-  );
-  const urlWithQuery = `${requestUrl}?${getQueryString(query.args)}`;
-
+  const requestUrl = `/${client.url}/${client.version}/${query.url}`
+    .replace(/\/+/g, '/') // remove duplicate slashes
+    .replace(/\/$/, ''); // remove tail slash
+  const queryString = getQueryString(query.args);
   const options: Request = {
     method: query.method,
     credentials: 'same-origin',
@@ -33,33 +30,18 @@ export default async function executeEndpoint(
       'Content-Type': 'application/json',
     },
   };
-
-  // If query string is too long, upgrade GET method to POST
-  if (query.method === 'GET') {
-    if ((location.host + urlWithQuery).length > 1000) {
-      options.method = 'POST';
-      // This doesn't make sense - stringifying data instead of args
-      // TODO: is this promotion/check even worth it
-      options.body = JSON.stringify(query.data);
-    } else {
-      requestUrl = urlWithQuery;
-    }
-  } else if (query.data) {
+  if (query.data) {
     options.body = JSON.stringify(query.data);
   }
 
-  console.log('endpoint:', {requestUrl, options});
-
   try {
-    const response = await fetch(requestUrl, options);
+    console.log(`endpoint: trying to fetch ${requestUrl}`, options);
+    const response = await fetch(
+      queryString ? `${requestUrl}?${queryString}` : requestUrl,
+      options,
+    );
 
-    /*
-    if (response.status < 200 || response.status >= 300) {
-      // If bad request (code 300 or higher), reject promise
-      throw new Error(response)
-    }
-    */
-    // Read json stream
+    // Let client deal with status codes
     return response.json();
   } catch (error) {
     // Log error in collapsed group
@@ -73,87 +55,79 @@ export default async function executeEndpoint(
 }
 
 // Map the keys of the args object to an array of encoded url components
-function getQueryString(args) {
+function getQueryString(args: {[string]: mixed}): string {
   return Object.keys(args)
     .sort()
-    .map(argName => getQueryPart(argName, args[argName]))
-    .map(({key, value}) => (key ? `${key}=${value}` : value))
+    .map(argName => argName in methods && methods[argName](args[argName]))
+    .filter(Boolean)
     .join('&')
     .replace(/&&/g, '&');
 }
 
-// TODO wtf
-function getQueryPart(argName, arg) {
-  let key = argName;
-  let value = null;
+type WhereArg = {
+  name: string,
+  op: WhereOps,
+  value: any,
+};
+type OrderArg = {
+  column: string,
+  direction?: string,
+};
+const methods = {
+  where(arg: WhereArg | Array<WhereArg>): string {
+    // where: [{ name: 'column_name', op: '=', value: 'value' }]
+    return asArray(arg)
+      .map(w => `where=${urlEncode(w)}`)
+      .join('&');
+  },
+  order(arg: OrderArg | Array<OrderArg>): string {
+    // order_by: [{ column: 'column_name', direction: 'asc|desc' }]
+    // TODO: random()?
+    const columnList = asArray(arg).map(col => {
+      const {column, direction = 'asc'} = col;
+      return direction !== 'asc' ? `-${column}` : `${column}`;
+    });
+    return `order_by=${encodeURIComponent(columnList.join(','))}`;
+  },
+  limit(arg: number): string | null {
+    // limit: number
+    const parsedNum = parseInt(arg);
+    if (!isNaN(parsedNum)) {
+      return `limit=${parsedNum}`;
+    } else {
+      return null;
+    }
+  },
+  offset(arg: number): string | null {
+    // offset: number
+    const parsedNum = parseInt(arg);
+    if (!isNaN(parsedNum)) {
+      return `order=${parsedNum}`;
+    } else {
+      return null;
+    }
+  },
+  evented(): string {
+    return 'session_id';
+  },
+  metaData(arg: any): string {
+    return `meta_data=${urlEncode(arg)}`;
+  },
+  args(arg: any): string {
+    return `args=${urlEncode(arg)}`;
+  },
+  exclude(arg: any): string {
+    return `exclude=${urlEncode(arg)}`;
+  },
+  include(arg: any): string {
+    return `include=${urlEncode(arg)}`;
+  },
+};
 
-  switch (argName) {
-    case 'where': // where: [{ name: 'column_name', op: '=', value: 'value' }]
-      arg = asArray(arg);
-      key = null;
-      value = arg
-        .map(w => {
-          const value = encodeURIComponent(JSON.stringify(w));
-          return `where=${value}`;
-        })
-        .join('&');
-      break;
-
-    case 'order_by': // order_by: [{ column: 'column_name', direction: 'asc|desc' }]
-      arg = asArray(arg);
-      const columnList = concatMap(arg, col => {
-        if (typeof col === 'string') {
-          return col;
-        } else {
-          if ('column' in col && 'direction' in col) {
-            const {column, direction} = col;
-            return direction !== 'asc' ? `-${column}` : `${column}`;
-          } else {
-            return Object.keys(col).map(columnName => {
-              return col[columnName] !== 'asc'
-                ? `-${columnName}`
-                : `${columnName}`;
-            });
-          }
-        }
-      });
-      value = encodeURIComponent(columnList.join(','));
-      break;
-
-    case 'limit': // limit: number
-    case 'offset': // offset: number
-      const parsedNum = parseInt(arg);
-      if (!isNaN(parsedNum)) {
-        value = parsedNum;
-      } else {
-        key = null;
-        value = null;
-      }
-      break;
-
-    case 'evented':
-      key = 'session_id';
-
-    case 'metaData': // eslint-disable-line no-fallthrough
-    case 'args':
-    case 'exclude':
-    case 'include':
-      value = encodeURIComponent(JSON.stringify(arg));
-      break;
-
-    default:
-      key = value = null;
-  }
-
-  return {key, value};
+function urlEncode(str: any): string {
+  return encodeURIComponent(JSON.stringify(str));
 }
 
-function asArray(arg) {
+function asArray(arg: any): any {
   return arg instanceof Array ? arg : [arg];
-}
-
-function concatMap(arr, fn) {
-  return arr.reduce((acc, item) => {
-    return acc.concat(fn(item));
-  }, []);
 }
