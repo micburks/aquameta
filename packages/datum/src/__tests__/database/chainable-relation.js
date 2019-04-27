51995b5651ba967cea0d7e429eb292fe4f4aef4b
Mickey Burks <brks.mck@gmail.com>
2019-04-27 11:13:25 -0700

    Fix tests, move fn to its own file

--- packages/datum/src/__tests__/database/chainable-relation.js
+++ packages/datum/src/__tests__/database/chainable-relation.js
@@ -1,5 +1,5 @@
 import test from 'tape';
-import {fn, relation} from '../../database/chainable.js';
+import {relation} from '../../database/chainable.js';
 
 test('#relation - throws when no arguments provided', t => {
   t.throws(relation, Error);
@@ -17,38 +17,5 @@ test('#relation - adds `public` schema when no schema provided', t => {
   const rel = relation('widget');
   t.is(rel.schemaName, 'public');
   t.is(rel.relationName, 'widget');
-  t.end();
-});
-
-test('#fn - throws when no arguments provided', t => {
-  t.throws(fn, Error);
-  t.end();
-});
-
-test('#fn - parses schema and relation names from argument', t => {
-  const func = fn('widget.transpile', {});
-  t.is(func.schemaName, 'widget');
-  t.is(func.fnName, 'transpile');
-  t.end();
-});
-
-test('#fn - adds `public` schema when no schema provided', t => {
-  const func = fn('uuid_generate_v4', {});
-  t.is(func.schemaName, 'public');
-  t.is(func.fnName, 'uuid_generate_v4');
-  t.end();
-});
-
-test('#fn - handles val arrays', t => {
-  const args = [1, 2, 3];
-  const func = fn('endpoint.create_session', args);
-  t.deepEqual(func.args.args.vals, args);
-  t.end();
-});
-
-test('#fn - handles kwargs object', t => {
-  const args = {name: 'my_name', new_session: true};
-  const func = fn('endpoint.create_session', args);
-  t.deepEqual(func.args.args.kwargs, args);
   t.end();
 });
--- packages/datum/src/__tests__/database/executable.js
+++ packages/datum/src/__tests__/database/executable.js
@@ -6,30 +6,36 @@ import {database} from '../../index.js';
 test('#http - parses request', t => {
   const req = {
     method: 'POST',
-    url: '/endpoint/relation/test/table?name=value',
+    url: '/endpoint/v1/relation/test/table?name=value',
     body: {body: true},
   };
 
   const executable = database.http(req);
 
   t.is(executable.method, 'POST');
-  t.is(executable.url, '/endpoint/relation/test/table');
+  t.is(executable.url, '/endpoint/v1/relation/test/table');
   t.deepEqual(executable.args, {name: 'value'});
   t.deepEqual(executable.data, {body: true});
   t.end();
 });
 
-test('#http - parses source url', t => {
+test('#http - parses source url into fn call', t => {
   const req = {
     url: '/db/widget/widget/Component.js',
   };
 
   const executable = database.http(req);
-  console.log(executable);
+
+  const expectedArgs = [
+    {
+      vals: ['widget', 'widget', 'js', 'Component'],
+    },
+  ];
 
   t.is(executable.method, 'GET');
-  t.is(executable.url, req.url);
+  t.is(executable.url, 'function/endpoint/source');
   t.deepEqual(executable.args, {
+    args: expectedArgs,
     source: true,
   });
   t.is(executable.data, null);
