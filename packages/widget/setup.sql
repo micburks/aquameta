07976d6680d4dbf3f3428f86945e4264fa441fb5
Mickey Burks <brks.mck@gmail.com>
2019-05-08 20:40:06 -0700

    Fix sql setup script

--- packages/widget/setup.sql
+++ packages/widget/setup.sql
@@ -1,14 +1,14 @@
 begin;
 create schema if not exists widget;
 
-create table widget.component (
+create table if not exists widget.component (
   id uuid not null default public.uuid_generate_v4() primary key,
   name text not null,
   js text default '// import {html} from "/db/widget/core/index.js";',
   docs text
 );
 
-create table widget.core (
+create table if not exists widget.core (
   id uuid not null default public.uuid_generate_v4() primary key,
   name text not null,
   js text default '// export default function() {}',
