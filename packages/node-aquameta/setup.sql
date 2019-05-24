c373e8c696d2ef40e84ad9d69fa90da8c8b136e2
Mickey Burks <brks.mck@gmail.com>
2019-05-24 00:45:09 -0700

    Update sitemap view have name and js column to be referenced in source url

--- packages/node-aquameta/setup.sql
+++ packages/node-aquameta/setup.sql
@@ -3,8 +3,8 @@ begin;
 alter table endpoint.resource add column if not exists js text;
 
 -- page middleware
-create view endpoint.sitemap as
-select r.path, m.mimetype, r.content
+create or replace view endpoint.sitemap as
+select replace(r.path, '/', '-') as name, r.path, m.mimetype, r.content, r.js
 from endpoint.resource r
   join endpoint.mimetype m on m.id=r.mimetype_id;
 
