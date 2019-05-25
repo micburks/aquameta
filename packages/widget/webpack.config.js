3e4eae587c46a94e99a67acac13717326bcda372
Mickey Burks <brks.mck@gmail.com>
2019-05-25 15:55:33 -0700

    Try out some webpack stuff

--- packages/widget/webpack.config.js
+++ packages/widget/webpack.config.js
@@ -1,11 +1,47 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
+const config = {
+  mode: 'development',
   entry: './src/datum-entry.js',
   output: {
-    library: 'datum',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
+    // library: 'datum',
+    // libraryTarget: 'umd',
+    // libraryExport: 'default',
   },
 };
+
+module.exports = [{
+  ...config,
+  target: 'node',
+  output: {
+    ...config.output,
+    filename: 'lib.node.js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    alias: {
+      'aquameta-datum': path.resolve(
+        __dirname,
+        'node_modules/aquameta-datum',
+        'dist-node-esm/index.js'
+      ),
+    },
+  },
+}, {
+  ...config,
+  target: 'web',
+  output: {
+    ...config.output,
+    filename: 'lib.browser.js',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    alias: {
+      'aquameta-datum': path.resolve(
+        __dirname,
+        'node_modules/aquameta-datum',
+        'dist-browser-esm/index.js'
+      ),
+    },
+  },
+}];
