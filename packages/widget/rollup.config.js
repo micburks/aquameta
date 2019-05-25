2a718b04abc792cb18d70e2132e7c4ef4a8eeeff
Mickey Burks <brks.mck@gmail.com>
2019-05-25 15:56:26 -0700

    Get rollup to work for datum, get rid of webpack

--- packages/widget/rollup.config.js
+++ packages/widget/rollup.config.js
@@ -1,25 +1,60 @@
 import babel from 'rollup-plugin-babel'
-import builtins from 'rollup-plugin-node-builtins'
 import commonjs from 'rollup-plugin-commonjs'
-import globals from 'rollup-plugin-node-globals'
 import resolve from 'rollup-plugin-node-resolve'
 
-export default {
-  input: 'src/widget.js',
+const resolveOptions = {
+  only: [
+    'ramda',
+    'aquameta-datum',
+    '@micburks/pg',
+    '@micburks/pg-pool',
+    /pg-.+/,
+    /postgres-.+/,
+    'xtend',
+    'node-fetch',
+    'unfetch'
+  ],
+};
+
+const config = {
   plugins: [
     babel(),
-    globals(),
-    builtins(),
-    resolve({
-      browser: true,
-      only: ['ramda', 'aquameta-datum', 'scope-css', 'dot', 'parse5']
-    }),
     commonjs({
       include: 'node_modules/**',
     })
   ],
   output: {
-    file: 'dist/build.js',
     format: 'es'
   }
 }
+
+export default [{
+  // browser
+  ...config,
+  input: './node_modules/aquameta-datum/dist-browser-esm/index.js',
+  output: {
+    ...config.output,
+    file: 'dist/lib.browser.js',
+  },
+  plugins: [
+    ...config.plugins,
+    resolve({
+      ...resolveOptions,
+      browser: true,
+    }),
+  ],
+}, {
+  // node
+  ...config,
+  input: './node_modules/aquameta-datum/dist-node-esm/index.js',
+  output: {
+    ...config.output,
+    file: 'dist/lib.node.js',
+  },
+  plugins: [
+    ...config.plugins,
+    resolve({
+      ...resolveOptions,
+    }),
+  ],
+}]
